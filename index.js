f88505add5217905b102aee0896706aed4c96001
jccostaa <dev.jeancosta@gmail.com>
2023-06-25 18:22:45 -0300

    finalização do projeto

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
-
 const api = axios.create({
-  baseURL: 'https://rickandmortyapi.com/api',
-});
-
-let pagina = 1;
-let totalPaginas = 1;
-
-function personagensInicio() {
+    baseURL: 'https://rickandmortyapi.com/api',
+  });
+  
+  let pagina = 1;
+  let totalPaginas = 1;
+  
+  function personagensInicio() {
     api.get('/character')
       .then((res) => {
         const personagens = res.data.results;
-        mostraPersonagem([personagens]);
-    })
+        mostraPersonagem();
+      })
       .catch((err) => {
         console.log(err);
-    });
-}
+      });
+  }
+  
   personagensInicio();
-
-function mostraPersonagem() {
-    
+  
+  function mostraPersonagem() {
     const searchInput = document.getElementById('searchInput').value;
     api.get(`/character?name=${searchInput}&page=${pagina}`)
       .then((res) => {
@@ -33,94 +32,118 @@ function mostraPersonagem() {
         console.log(err);
       });
   }
-
-function mostrarInformacoes() {
+  
+  function mostrarInformacoes() {
     const personagensInfo = document.getElementById('personagensInfo');
     personagensInfo.textContent = `Página ${pagina} de ${totalPaginas}`;
   }
   
-function mostrarCards(personagens) {
+  
+  function mostrarCards(personagens) {
     const containerCards = document.getElementById('container-cards');
-    containerCards.innerHTML = '';
+    let html = '';
   
-personagens.forEach((personagem) => {
-    const article = document.createElement('article');
-    article.className = 'card';
-    const img = document.createElement('img');
-    img.className = 'img-personagem';
-    img.src = personagem.image;
-    img.alt = personagem.name;
-  
-    const textoCard = document.createElement('div');
-    textoCard.className = 'texto-card';
-  
-    const nome = document.createElement('h1');
-    nome.textContent = personagem.name;
-    
-    const genero = document.createElement('h4');
-    genero.textContent = personagem.gender;
-
-    const status = document.createElement('h4');
-    status.textContent = `${personagem.status} ${personagem.species}`;
-    
-    const ultimoLocal = document.createElement('p');
-    ultimoLocal.textContent = `Ultimo local conhecido: ${personagem.location.name}`
-
-
-    textoCard.appendChild(nome);
-    textoCard.appendChild(status);
-    textoCard.appendChild(genero);
-    textoCard.appendChild(ultimoLocal);
-  
-    article.appendChild(img);
-    article.appendChild(textoCard);
-  
-    containerCards.appendChild(article);
-    });
-}
-
-function statusCor(status){
-    switch (status) {
-        case 'Alive':
-            return 'green';
-        case 'Dead':
-            return 'red';
-        default:
-            return 'gray';
+    personagens.forEach((personagem) => {
+        html += `
+          <button id="id-${personagem.id}" onclick="openModal(${personagem.id})" class="card p-0 mb-3 text-bg-dark" style="max-width: 540px;">
+            <div class="row g-0">
+              <div class="col-4">
+                <img src="${personagem.image}" class="img-fluid img-personagem rounded-start" alt="Imagem do personagem">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body d-flex flex-column align-items-start">
+                  <h5 class="card-title">${personagem.name}</h5>
+                  <div class="d-flex align-items-center">
+                    <div class="status-personagem ${statusCor(personagem.status)}"></div>
+                    <p class="card-text">${personagem.status} - ${personagem.species}</p>
+                  </div>
+                  <p class="card-text">${personagem.gender}</p>
+                  <p class="card-text"><small class="text-bg-dark">Última localização: ${personagem.location.name}</small></p>
+                </div>
+              </div>
+            </div>
+          </button>
+        `;
+      });
+      containerCards.innerHTML = html;
     }
-}
+  
+    function openModal(personagemId) {
+        api.get(`/character/${personagemId}`)
+          .then((res) => {
+            const personagem = res.data;
+            const modalContent = `
+              <div class="modal-dialog">
+                <div class="modal-content text-bg-dark">
+                  <div class="modal-header">
+                    <h5 class="modal-title">${personagem.name}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body d-flex justify-content-center flex-column align-items-center">
+                    <img src="${personagem.image}" class="img-fluid imagem-modal" alt="Imagem do personagem">
+                    <p>Status: ${personagem.status}</p>
+                    <p>Espécie: ${personagem.species}</p>
+                    <p>Origem: ${personagem.origin.name}
+                    <p>Gênero: ${personagem.gender}</p>
+                    <p>Última localização: ${personagem.location.name}</p>
+                  </div>
+                </div>
+              </div>
+            `;
+            const modalElement = document.getElementById('modal-personagem');
+            modalElement.innerHTML = modalContent;
+            const modalInstance = new bootstrap.Modal(modalElement);
+            modalInstance.show();
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }
 
-function paginaAnterior() {
+  function statusCor(status) {
+    switch (status) {
+      case 'Alive':
+        return 'bg-success';
+      case 'Dead':
+        return 'bg-danger';
+      default:
+        return 'bg-secondary';
+    }
+  }
+
+  function paginaAnterior() {
     if (pagina > 1) {
       pagina--;
       mostraPersonagem();
     }
   }
   
-function proximaPagina() {
+  function proximaPagina() {
     if (pagina < totalPaginas) {
       pagina++;
       mostraPersonagem();
     }
-}
-
-function informacoes(){
+  }
+  
+  function informacoes() {
     axios.all([
-        api.get('/character'),
-        api.get('/location'),
-        api.get('/episode')
-      ])
-        .then(axios.spread(function (characterResponse, locationResponse, episodeResponse) {
-          const characterCount = characterResponse.data.info.count;
-          const locationCount = locationResponse.data.info.count;
-          const episodeCount = episodeResponse.data.info.count;
-    
-          document.getElementById('personagensTotal').textContent = `PERSONAGENS: ${characterCount}`;
-          document.getElementById('localizacoesTotal').textContent = `LOCALIZAÇÕES: ${locationCount}`;
-          document.getElementById('episodiosTotal').textContent = `EPISÓDIOS: ${episodeCount}`;
-        }))
-        .catch(function (error) {
-          console.log(error);
-        });
-    }
-    informacoes();
+      api.get('/character'),
+      api.get('/location'),
+      api.get('/episode')
+    ])
+      .then(axios.spread(function (characterResponse, locationResponse, episodeResponse) {
+        const characterCount = characterResponse.data.info.count;
+        const locationCount = locationResponse.data.info.count;
+        const episodeCount = episodeResponse.data.info.count;
+  
+        document.getElementById('personagensTotal').textContent = `PERSONAGENS: ${characterCount}`;
+        document.getElementById('localizacoesTotal').textContent = `LOCALIZAÇÕES: ${locationCount}`;
+        document.getElementById('episodiosTotal').textContent = `EPISÓDIOS: ${episodeCount}`;
+      }))
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+  
+  informacoes();
+  
